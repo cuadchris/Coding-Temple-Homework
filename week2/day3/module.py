7a73851a859216c21ffbb2dad9004a7e0eb8e75e
Christopher <cuadradochris@hotmail.com>
2022-09-29 11:13:26 -0400

    might be complete

--- week2/day3/module.py
+++ week2/day3/module.py
@@ -1,5 +1,12 @@
-def houseArea():
+from math import pi
+
+def squareFootage():
     length = int(input("Enter the length of your home in feet: "))
     width = int(input("Now the width: "))
     area = length * width
-    return f'Your home is {area} square feet!'
+    print(f'Your home is {area} square feet!')
+
+def circumference():
+    radius = int(input("Enter the radius: "))
+    circumference = 2 * pi * radius
+    print(f'The circumference is {circumference}')
