4519e1bd17e6f276006dffc45a61ef70c5fbe1cb
Christopher <cuadradochris@hotmail.com>
2022-10-15 18:49:59 -0400

    more work

--- week4/day4/Pokedex.py
+++ week4/day4/Pokedex.py
@@ -6,25 +6,13 @@ import sys
 class Pokedex():
     pokedata = []
 
-    # This function exists in case you'd like to quickly add pokemon to your pokedex.
+    # This function exists in case you'd like to quickly add pokemon to your pokedex. It takes an
+    # integer for how many you'd like to add.
 
-    def fillPokedex(self):
-        for i in range(5):
+    def fillPokedex(self, amount):
+        for i in range(amount):
             random_pokemon_id = random.randint(1, 300)
             self.addPokemon(random_pokemon_id)
-            # url = f'https://pokeapi.co/api/v2/pokemon/{random_pokemon_id}'
-            # response = r.get(url)
-            # if not response.ok:
-            #     return "Error!"
-            # data = response.json()
-            # pokedict = {}
-            # pokename = data['name']
-            # pokedict[pokename] = {
-            #     'abilities': [ability['ability']['name'] for ability in data['abilities']],
-            #     'base_exp': data['base_experience'],
-            #     'f_shiny': data['sprites']['front_shiny']
-            # }
-            # self.pokedata.append(pokedict)
 
     def addPokemon(self, pokemon):
         url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
@@ -56,7 +44,8 @@ class Pokedex():
 
     def catchPokemon(self):
         pokemon = input("Enter the Pokemon you'd like to catch: ").lower()
-        chances = [True]
+        # You only have a 33% chance to actually catch the Pokemon you want. Sorry!, lol.
+        chances = [True, False, False]
         result = random.choice(chances)
         self.pokePrint("You used POKe´BALL!\n", .08)
         time.sleep(1.5)
@@ -73,30 +62,14 @@ class Pokedex():
             return
         self.pokePrint(f'{pokemon.capitalize()} escaped!\n', .03)
 
+    # That classic Gameboy Pokemon text. Use the speed argument to play around!
     def pokePrint(self, string, speed):
         for char in string:
             sys.stdout.write(char)
             sys.stdout.flush()
             time.sleep(speed)
 
-
-        # url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
-        # response = r.get(url)
-        # if not response.ok:
-        #     return "Error!"
-        # data = response.json()
-        # pokedict = {}
-        # pokedict[pokemon] = {
-        #     'abilities': [ability['ability']['name'] for ability in data['abilities']],
-        #     'base_exp': data['base_experience'],
-        #     'f_shiny': data['sprites']['front_shiny']
-        # }
-        # self.pokedata.append(pokedict)
-
-p1 = Pokedex()
-# p1.catchPokemon()
-# p1.fillPokedex()
-# p1.fillPokedex()
-# p1.showPokedex()
-# print(len(p1.pokedata))
-p1.catchPokemon()
+pokedex = Pokedex()
+# pokedex.fillPokedex(5) < Use this to quick add.
+# p1.catchPokemon() < Use this to catch a Pokemon!
+# pokedex.showPokedex() < Show off your Codex!
